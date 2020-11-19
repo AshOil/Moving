8f61975eef7dc6b455b44250a0f1a479c3874554
AshOil <ka0@kakao.com>
2020-11-19 23:05:27 +0900

    MovieDetail

--- final-front/src/views/moviecontents/MovieDetail.vue
+++ final-front/src/views/moviecontents/MovieDetail.vue
@@ -1,17 +1,34 @@
 <template>
-  <div>
-    <img src="https://i.ytimg.com/vi/448Q1YrnhAs/maxresdefault.jpg" alt="">
+  <div @click="onClick">
+    <h1>{{ moviedata.title }}</h1>
     <Article />
   </div>
 </template>
 
 <script>
 import Article from '../../components/Article.vue'
+import {mapState} from 'vuex'
 
 export default {
   name: 'MovieDetail',
+  data() {
+    return {
+      moviedata: this.movie
+    }
+  },
   components: {
     Article,
+  },
+
+  computed: {
+    ...mapState([
+      'movie'
+    ])
+  },
+  beforeCreated() {
+    // this.moviedata = this.movie
+    this.$store.dispatch('emptyMovie')
+    console.log(this.moviedata)
   }
 
 }
