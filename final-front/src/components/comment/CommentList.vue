39047f0440ff1c80de266c379d31598950a66859
minju <baiktte@naver.com>
2020-11-24 12:52:41 +0900

    comment 생성리스트

---
+++ final-front/src/components/comment/CommentList.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <CreateComment 
+      :movieid="movieid" 
+      :article="article" 
+      @create-comment-input = createdComment
+    />
+    <Comment
+      v-for="comment in comments" :key='comment.id'
+      :movieid="movieid" 
+      :article="article" 
+      :comment="comment"
+      @delete-comment = deleteComment
+
+    />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+import CreateComment from '../comment/CreateComment'
+import Comment from '../comment/Comment'
+
+export default {
+  name: 'CommentList',
+  data() {
+    return {
+      comments: []
+    }
+  },
+  components: {
+    CreateComment,
+    Comment
+  },
+  props: {
+    article: {
+      type: Object,
+    },
+    movieid: {
+      type: Number,
+    }
+  },
+  methods: {
+    setToken() {
+      const token =localStorage.getItem('jwt')
+      const config = {
+        headers:{
+          Authorization: `JWT ${token}`
+        }
+      }
+      return config
+    },
+    getComments() {
+      console.log(this.comments)
+      // Comment List 가져와
+      const config = this.setToken()
+      // const commentItem = {
+      //   article_id: this.article.id,
+      //   movie_id: this.movieid
+      // }
+      console.log(config)
+      axios.get(`http://127.0.0.1:8000/moviedata/${this.movieid}/articles/${this.article.id}/comments/`, config)
+      .then(res => {
+        console.log(res.data)
+        this.comments = res.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    },
+    createdComment(text) {
+      console.log(text)
+      this.comments.push(text)
+      console.log(this.comments)
+    },
+    deleteArticle(commentId) {
+      this.comments = this.comments.filter(comment => {
+        return comment.id !== commentId
+      })
+    },
+  },
+  created() {
+    this.getComments()
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ final-front/src/components/comment/CreateComment.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <span>
+      <b-icon class="d-inline" icon="chat-left-text-fill"></b-icon>
+      <input type="text" v-model="content" placeholder="댓글써라" @keypress.enter="createComment">
+    </span>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'CreateComment',
+  props: {
+    article: {
+      type: Object
+    },
+    movieid: {
+      type : Number
+    }
+  },
+  data() {
+    return {
+      content: '',
+    }
+  },
+  methods: {
+    setToken() {
+      const token =localStorage.getItem('jwt')
+      const config = {
+        headers:{
+          Authorization: `JWT ${token}`
+        }
+      }
+      return config
+    },
+    createComment() {
+      const config = this.setToken()
+      const commentItem = {
+        content: this.content,
+        movie_id: this.movieid,
+        article_id: this.article.id
+      }
+      console.log(commentItem)
+      axios.post(`http://127.0.0.1:8000/moviedata/${this.movieid}/articles/${this.article.id}/comments/`, commentItem, config)
+        .then(res => {
+          // 반응형 변경!!
+          this.$emit('create-comment-input', res.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
