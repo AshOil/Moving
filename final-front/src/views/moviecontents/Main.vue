5a82655dc1d7f94b8f4fc45e8fe100f1bfde2a09
AshOil <ka0@kakao.com>
2020-11-25 21:47:08 +0900

    moving?

--- final-front/src/views/moviecontents/Main.vue
+++ final-front/src/views/moviecontents/Main.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="login">
+  <div   id="goleft" v-if="login">
       <div id="nav" class="p-3">
         <div style="width: 1220px; margin: 0 auto ">
           <nav class="navbar navbar-expand-lg navbar-light" style=" height: 40px;margin: 10px;margin-bottom: 30px;">
@@ -25,16 +25,53 @@
                     @click.native="logout">Logout</router-link
                   >
                 </li>
+                <li class="nav-item">
+                  <a style="background-color: #14181b; color:#f0ad4e" class="nav-link" 
+                    @click="openNav()">Moving?</a>
+                </li>
               </ul>
             </div>
           </nav>
+           <div>
             <MovieList />
+           </div>
         </div>
-      </div>
-      
+    <div id="mySidenav" class="sidenav">
+        <div style="color:white">
+          <hr style="border: solid 1px #f0ad4e;">
+          <p style=" font-weight:bold"> MOVING은 뭔가요? </p>
+          <hr style="border: solid 1px #f0ad4e;">
+          <p style="display:block">
+            Moving은 이미지를 활용한 <br> 
+            영화 추천 사이트입니다. <br> 
+              <br> 
+            여러 과정 필요없이, <br> 
+            한눈의 평점을 비교하고 확인할 수 있습니다.<br> 
+            <br> 
+            포스터의 크기는 다른 이용자의<br>
+            평점을 의미합니다. 
+            <br>
+            <br>
+            포스터가 클수록 <br> 
+            사용자들에게 우수한 평가를 받은 영화입니다.<br> 
+
+          </p>
+          <hr style="border: solid 1px #f0ad4e;">
+          <p style=" font-weight:bold"> 흔들리는 포스터는 뭔가요? </p>
+          <hr style="border: solid 1px #f0ad4e;">
+          <p style="display:block">
+            당신이 최근 좋게 평가한 영화를  <br>
+            기준으로 맞춤 추천된 영화입니다. <br>
+            <br>
+            혹시 흔들리는 포스터가 없다면, <br>
+            좋아하는 영화에 평가를 남겨보세요! <br>
+            더 좋은 영화를 추천받을 수 있을거에요.
+          </p>
+          <a href="javascript:void(0)" class="closebtn" @click="closeNav()">&times;</a>
+        </div>
     </div>
-
-
+      </div>
+    </div>
 </template>
 
 <script>
@@ -53,6 +90,16 @@ export default {
       this.$store.dispatch('emptyUser')
       localStorage.removeItem('jwt')
       this.$router.push({name: 'Home' })
+    },
+    openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("goleft").style.marginRight = "250px";
+      // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    },
+    closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+      document.getElementById("goleft").style.marginRight = "0";
+      // document.body.style.backgroundColor = "white";
     }
   },
   created() {
@@ -90,4 +137,33 @@ export default {
   color: #42b983;
 }
 
+.sidenav {
+  height: 100%; /* 100% Full-height */
+  width: 0; /* 0 width - change this with JavaScript */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Stay on top */
+  top: 0; /* Stay at the top */
+  right: 0;
+  background-color: #111; /* Black*/
+  overflow-x: hidden; /* Disable horizontal scroll */
+  padding-top: 60px; /* Place content 60px from the top */
+  transition: 0.5s; /* 0.5 second transition effect to slide in the sidenav */
+}
+
+/* The navigation menu links */
+
+/* Position and style the close button (top right corner) */
+.sidenav .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+}
+
+/* Style page content - use this if you want to push the page content to the right when you open the side navigation */
+#goleft {
+  transition: margin-right .5s;
+  padding: 20px;
+}
 </style>
